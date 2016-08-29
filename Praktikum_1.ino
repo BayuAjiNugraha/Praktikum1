ded152dba0da58ed7d1b129176560b2cfdf54a9d
BayuAjiNugraha <bayuajingrh@yahoo.com>
2016-08-29 08:47:21 -0700

    Update Praktikum_1.ino

--- Praktikum_1.ino
+++ Praktikum_1.ino
@@ -8,7 +8,7 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   digitalWrite(led, HIGH);
-  delay(1000);
+  delay(200);
   digitalWrite(led, LOW);
-  delay(1000);
+  delay(500);
 }
